c62a39114e4d3a2741c79f0808510d6d22b107d7
Lucas A Jones <lucas.a.jones406@gmail.com>
2018-06-20 17:33:12 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+'use strict';
+   
+$ ( document ).ready(function() {
+   $( "a" ).click(function( event ) {
+       alert( "the link will no longer take you jquery.com" );
+       even.preventDefault();
+    });
+})
+
