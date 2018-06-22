62355f93972edebfe75872b91cd3b8da2d299157
Lucas A Jones <lucas.a.jones406@gmail.com>
2018-06-22 11:05:29 -0600

    added circle with click

--- main.js
+++ main.js
@@ -3,9 +3,32 @@
    
 // shorthand version is  $( function() {
 $ ( document ).ready(function() {
-   var canvas = new Canvas($('canvas')[0]),
+    var canvas = new Canvas($('canvas')[0]),
         debug = false,
         mouseDown = false;
+
+    $("#canvas").click(function (e) {
+        var x = Math.floor((e.pageX - $("#canvas").offset().left)),
+            y = Math.floor((e.pageY - $("#canvas").offset().top));
+    //    waterfall.wayPoint = new Vector(x, y);
+    });
+   
+    $("#canvas").mousedown(function () {
+        mouseDown = true;
+    });
+
+    $(document).mouseup(function () {
+        mouseDown = false;
+    });
+
+    $("#canvas").mousemove(function (e) {
+        if (mouseDown === false) {
+            return;
+        }
+        var x = Math.floor((e.pageX - $("#canvas").offset().left)),
+            y = Math.floor((e.pageY - $("#canvas").offset().top));
+      //  waterfall.wayPoint = new Vector(x, y);
+    });
 
 });
 
