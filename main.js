1bf627b6b05e8a21ff11294713f9308bc99de150
Lucas A Jones <lucas.a.jones406@gmail.com>
2018-06-22 10:50:26 -0600

    added canvas

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 
 'use strict';
    
+// shorthand version is  $( function() {
 $ ( document ).ready(function() {
-   $( "a" ).click(function( event ) {
-       alert( "the link will no longer take you jquery.com" );
-       even.preventDefault();
-    });
-})
+   
+});
 
