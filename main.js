f793bce3ca369f3a1e2f96ec0a3ce04f6187ebb6
Lucas A Jones <lucas.a.jones406@gmail.com>
2018-06-22 12:24:09 -0600

    added waypoint functionality

--- main.js
+++ main.js
@@ -2,15 +2,23 @@
 'use strict';
    
 // shorthand version is  $( function() {
+// This tells JS not to execute until the html page is loaded
 $ ( document ).ready(function() {
     var canvas = new Canvas($('canvas')[0]),
+        model = new Model(canvas),
         debug = false,
         mouseDown = false;
+    if (debug) {
+        model.update();
+    } else {
+        setInterval(model.update.bind(model), 24);
+    }
+
 
     $("#canvas").click(function (e) {
         var x = Math.floor((e.pageX - $("#canvas").offset().left)),
             y = Math.floor((e.pageY - $("#canvas").offset().top));
-    //    waterfall.wayPoint = new Vector(x, y);
+        model.wayPoint = new Vector(x, y);
     });
    
     $("#canvas").mousedown(function () {
@@ -27,8 +35,39 @@ $ ( document ).ready(function() {
         }
         var x = Math.floor((e.pageX - $("#canvas").offset().left)),
             y = Math.floor((e.pageY - $("#canvas").offset().top));
-      //  waterfall.wayPoint = new Vector(x, y);
+        model.wayPoint = new Vector(x, y);
     });
 
 });
+
+
+var Model = function (canvas) {
+    
+    var ModelDomainWidth = 400;
+    this.originX = canvas.width / 2 - waterfallWidth / 2;
+    this.originY = 0;
+    this.ModelDomainWidth = ModelDomainWidth;
+    this.canvas = canvas;
+    this.wayPoint = new Vector(canvas.width / 2, this.originY + 200);
+
+};
+
+
+Model.prototype = {
+    
+    update: function () {
+
+        this.canvas.clear();
+        
+        this.drawWayPoint();
+        
+    },
+
+
+    drawWayPoint: function () {
+        var alpha = 1, color = 'rgba(0,153,255,' + alpha + ')';
+        this.canvas.circle(this.wayPoint.x, this.wayPoint.y, 5, color);
+    }
+
+};
 
