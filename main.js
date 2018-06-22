2630947f1a325f8b2ec2650f8bc3d0597a6ba498
Lucas A Jones <lucas.a.jones406@gmail.com>
2018-06-22 13:06:06 -0600

    update

--- main.js
+++ main.js
@@ -14,7 +14,8 @@ $ ( document ).ready(function() {
         setInterval(model.update.bind(model), 24);
     }
 
-
+    model.wayPoint = new Vector(x, y);
+/*
     $("#canvas").click(function (e) {
         var x = Math.floor((e.pageX - $("#canvas").offset().left)),
             y = Math.floor((e.pageY - $("#canvas").offset().top));
@@ -37,7 +38,7 @@ $ ( document ).ready(function() {
             y = Math.floor((e.pageY - $("#canvas").offset().top));
         model.wayPoint = new Vector(x, y);
     });
-
+*/
 });
 
 
