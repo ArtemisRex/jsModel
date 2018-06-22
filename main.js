32b05da1253470e9c0cb3423f5e6a5cf04d18ac9
Lucas A Jones <lucas.a.jones406@gmail.com>
2018-06-22 11:01:09 -0600

    added canvas functionality

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
    
 // shorthand version is  $( function() {
 $ ( document ).ready(function() {
-   
+   var canvas = new Canvas($('canvas')[0]),
+        debug = false,
+        mouseDown = false;
+
 });
 
